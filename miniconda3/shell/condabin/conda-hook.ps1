51a2a98722f7c0b04dccff964554d83d1cbf3b6e
cwarren <warrenchrisb@gmail.com>
2025-03-19 01:26:39 +0000

    add todo

--- miniconda3/shell/condabin/conda-hook.ps1
+++
@@ -1,9 +0,0 @@
-$Env:CONDA_EXE = "/home/ubuntu/sage-bio/ad-multi-omics-pipeline/miniconda3/bin/conda"
-$Env:_CE_M = $null
-$Env:_CE_CONDA = $null
-$Env:_CONDA_ROOT = "/home/ubuntu/sage-bio/ad-multi-omics-pipeline/miniconda3"
-$Env:_CONDA_EXE = "/home/ubuntu/sage-bio/ad-multi-omics-pipeline/miniconda3/bin/conda"
-$CondaModuleArgs = @{ChangePs1 = $True}
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
-
-Remove-Variable CondaModuleArgs
